677c9ac9879228f378685406a50fa45ff966b487
Jovan Kelly <jovanjkelly@yahoo.com>
2016-07-27 11:45:37 -0400

    database change

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    production: { url: "mongodb://localhost:27017/test"},
-    dev: { url: 'ds027145.mlab.com:27145/cypresshills'}
+    production: { url: 'ds027145.mlab.com:27145/cypresshills'},
+    dev: { url: "mongodb://localhost:27017/test"}
 }
