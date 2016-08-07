68832f321a3668268be711848734d1b5177aa7a0
Aaron <Aaron>
2016-08-07 16:02:04 -0400

    listing => jobs2: routes to (Job/index); limited fields sent

--- controllers/jobListing.js
+++ controllers/jobListing.js
@@ -5,7 +5,7 @@ var express = require('express'),
 router.get('/jobListing', function(req, res){
     Job.find({}, function(err, jobs){
         if(err) throw err;
-        // res.send(jobs);
+
         res.render('jobListings', {
             results: jobs,
             user: req.user
